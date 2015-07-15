d294a9d13e105db2153dcd5b6c241e3b83a8cb71
Willis, Sylvester <Sylvester.Willis@gtri.gatech.edu>
2015-07-15 08:00:34 -0400

    Added portfolio page using the start bootstrap theme on http://startbootstrap.com/template-overviews/freelancer/.

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
-var ItemApi = require('./app/routes/item');
+var path       = require("path");
+var ItemApi    = require('./app/routes/item');
 
 mongoose.connect('mongodb://localhost:27017/inven-manage');
 
@@ -11,18 +12,17 @@ mongoose.connect('mongodb://localhost:27017/inven-manage');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//setup static directories
+app.use("/vendor", express.static(__dirname + '/vendor'));
+app.use("/static", express.static(__dirname + '/static'));
+
 var port = process.env.PORT || 8080;        // set our port
 
 var router = express.Router();              // get an instance of the express Router
 
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
-});
 
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    res.sendfile(path.join(__dirname+'/index.html'));
 });
 
 // REGISTER OUR ROUTES -------------------------------
