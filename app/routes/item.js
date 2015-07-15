542e49d47579797a7ec7cb017d949f6c8421630a
Sylvester Willis <Sylvester.Willis@gtri.gatech.edu>
2015-07-14 23:24:36 -0400

    Moved Item route to separate directory.

---
+++ app/routes/item.js
@@ -0,0 +1,77 @@
+var express    = require('express');
+var Item       = require('../models/item');
+
+module.exports = (function() {
+    'use strict';
+    var itemApi = express.Router();
+
+    itemApi.route('/items')
+    // create a bear (accessed at POST http://localhost:8080/api/items)
+    .post(function(req, res) {
+            
+        var item = new Item();      // create a new instance of the Item model
+        item.name = req.body.name;  // set the item name (comes from the request)
+        item.quantityInStock = req.body.quantityInStock;
+        item.quantityGiven = 0;
+
+        // save the item and check for errors
+        item.save(function(err) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json(req.body);
+        });
+            
+    })
+    .get(function(req, res) {
+        Item.find(function(err, items) {
+            if(err) {
+                res.send(err);
+            }
+
+            res.json(items);
+        });
+    });
+
+    itemApi.route('/items/:item_id')
+    .get(function(req, res) {
+        Item.findById(req.params.item_id, function(err, item){
+            if(err) {
+                res.send(err);
+            }
+
+            res.json(item);
+        });
+    })
+    .put(function(req, res) {
+        Item.findById(req.params.item_id, function(err, item) {
+            if(err) {
+                res.send(err);
+            }
+
+            item.name = req.body.name;  // set the item name (comes from the request)
+            item.quantityInStock = req.body.quantityInStock;
+            item.quantityGiven = 0;
+
+            item.save(function(err) {
+                if(err) {
+                    res.send(err);
+                }
+
+                res.json({ message: 'Item updated!' });
+            });
+        });
+    })
+    .delete(function(req, res) {
+        Item.remove({ _id: req.params.item_id }, function(err, item) {
+            if(err) {
+                res.send(err);
+            }
+
+            res.json({ message: 'Successfully deleted.' });
+        });
+    });
+
+    return itemApi;
+})();
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
-var Item       = require('./app/models/item');
+var ItemApi = require('./app/routes/item');
 
 mongoose.connect('mongodb://localhost:27017/inven-manage');
 
@@ -25,76 +25,10 @@ router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-router.route('/items')
-// create a bear (accessed at POST http://localhost:8080/api/bears)
-.post(function(req, res) {
-        
-    var item = new Item();      // create a new instance of the Bear model
-    item.name = req.body.name;  // set the bears name (comes from the request)
-    item.quantityInStock = req.body.quantityInStock;
-    item.quantityGiven = 0;
-
-    // save the bear and check for errors
-    item.save(function(err) {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json(req.body);
-    });
-        
-})
-.get(function(req, res) {
-    Item.find(function(err, items) {
-        if(err) {
-            res.send(err);
-        }
-
-        res.json(items);
-    });
-});
-
-router.route('/items/:item_id')
-.get(function(req, res) {
-    Item.findById(req.params.item_id, function(err, item){
-        if(err) {
-            res.send(err);
-        }
-
-        res.json(item);
-    });
-})
-.put(function(req, res) {
-    Item.findById(req.params.item_id, function(err, item) {
-        if(err) {
-            res.send(err);
-        }
-
-        item.name = req.body.name;  // set the bears name (comes from the request)
-        item.quantityInStock = req.body.quantityInStock;
-        item.quantityGiven = 0;
-
-        item.save(function(err) {
-            if(err) {
-                res.send(err);
-            }
-
-            res.json({ message: 'Item updated!' });
-        });
-    });
-})
-.delete(function(req, res) {
-    Item.remove({ _id: req.params.item_id }, function(err, item) {
-        if(err) {
-            res.send(err);
-        }
-
-        res.json({ message: 'Successfully deleted.' });
-    });
-});
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
+app.use('/api', ItemApi);
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
