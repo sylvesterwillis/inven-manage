aaae6f80c5b86588c572cf13d72a8648a32aba21
Willis, Sylvester <Sylvester.Willis@gtri.gatech.edu>
2015-07-21 16:38:48 -0400

    Issue 1: Added ESHint since it has better JSX support than JSHint and after further evaluation decided not to use JSCS due to some style conflicts with ESHint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,61 @@
 module.exports = function(grunt) {
 
-  // Load the plugins.
-  grunt.loadNpmTasks('grunt-react');
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-babel');
+    // Load the plugins.
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-react');
 
-  // Project configuration.
-  grunt.initConfig({
-    browserify: {
-      options: {
-        transform: ["babelify"],
-        browserifyOptions: {
-          paths: [
-            "./static/js/frontend/index"
-          ],
-          extensions: [".js", ".json", ".jsx"]
+    // Project configuration.
+    grunt.initConfig({
+        eslint: {
+            target: ['static/js/frontend/**/*.js']
+        },
+        browserify: {
+            options: {
+                transform: ["babelify"],
+                browserifyOptions: {
+                    paths: [
+                        "./static/js/frontend/index"
+                    ],
+                    extensions: [".js", ".json", ".jsx"]
+                }
+            },
+            build: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: "./static/js/frontend/",
+                        src: "*",
+                        dest: "./static/js/dist/",
+                        ext: ".js"
+                    }
+                ]
+            }
+        },
+        react: {
+            combined_file_output: {
+                files: {
+                    './static/js/dist/index.js': [
+                        './static/js/frontend/index/items.js'
+                    ]
+                }
+            },
+            dynamic_mappings: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: './static/js/frontend',
+                        src: ['**/*.jsx'],
+                        dest: './static/js/dist/dest',
+                        ext: '.js'
+                    }
+                ]
+            }
         }
-      },
-      build: {
-        files: [{
-          expand: true,
-          cwd: "./static/js/frontend/",
-          src: "*",
-          dest: "./static/js/dist/",
-          ext: ".js"
-        }]
-      }
-    },
-    react: {
-      combined_file_output: {
-        files: {
-          './static/js/dist/index.js': [
-            './static/js/frontend/index/items.js'
-            ]
-        }
-      },
-      dynamic_mappings: {
-        files: [
-          {
-            expand: true,
-            cwd: './static/js/frontend',
-            src: ['**/*.jsx'],
-            dest: './static/js/dist/dest',
-            ext: '.js'
-          }
-        ]
-      }
-    }
-  });
+    });
 
-  grunt.registerTask('default', ['react']);
-  grunt.registerTask('prod', ['browserifyProd']);
+    grunt.registerTask('default', ['eslint', 'react']);
+    grunt.registerTask('prod', ['eslint', 'browserifyProd']);
 
 };
